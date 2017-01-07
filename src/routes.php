cd05792cfcb8ee71bb11c008a32e728d7d790bed
ll9m <leo.j.liu@newegg.com>
2017-01-07 17:21:25 +0800

    add adminlte template

--- src/routes.php
+++ src/routes.php
@@ -3,12 +3,12 @@
 
 $app->get('/', function ($request, $response, $args) {
     $this->logger->info("Slim-Skeleton '/' route");
-    return $this->view->render($response, 'index.html',[]);
+    return $this->view->render($response, '/front/index.html',[]);
 });
 
-$app->group('/customers',function (){
-   $this->get('/add',function ($request,$response,$args){
-       return $this->view->render($response, '/customers/add.html',[]);
+$app->group('/control-panel',function (){
+   $this->get('/index',function ($request,$response,$args){
+       return $this->view->render($response, '/admin/index.html',[]);
    });
 });
 
